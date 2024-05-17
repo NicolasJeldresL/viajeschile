54a8dc95f3c846c470a07d3c64b7572311c79cb6
NicolasJeldresL <nicolasjeldreslazoi@gmail.com>
2024-05-16 21:12:20 -0400

    Tooltip y Alert agregado al botón formulario

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();
+    $('#formulario').click(function(){
+      alert("Formulario enviado correctamente, responderemos a la brevedad");
+    });
+});
